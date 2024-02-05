53f229b99cb4367ac52544407733feddada33d73
서우승 <148739245+SeoWooSeung@users.noreply.github.com>
2024-02-05 18:06:47 +0900

    Add files via upload

--- JS/custom.js
+++ JS/custom.js
@@ -1,31 +1,39 @@
 $(function () {
+  $(".main_slide").slick({
+    arrows: true,
+    autoplay: false,
+    pauseOnHover: false,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    // fade: true,
+  });
 
-    $('.main_slide').slick({
-        arrows: true,
-        autoplay: false,
-        pauseOnHover: false,
-        dots: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        // fade: true,
-    });
+  $(".btn").on("click", function () {
+    $(".right_banner").toggleClass("on");
+  });
 
-    $('.btn').on('click', function () {
-        $('.right_banner').toggleClass('on')
-    })
+  $(".totop").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+  });
 
-    $('.totop').on('click', function () {
-        $('html, body').animate({ scrollTop: 0 }, 800)
-    });
+  $(window).on("scroll", function () {
+    const sct = $(window).scrollTop();
+    if (sct > 800) {
+      $(".totop").addClass("on");
+      $(".header").addClass("on");
+    } else {
+      $(".totop").removeClass("on");
+      $(".header").removeClass("on");
+    }
+  });
 
-    $(window).on('scroll', function () {
-        const sct = $(window).scrollTop();
-        if (sct > 800) {
-            $('.totop').addClass('on');
-            $('.Header').addClass('on');
-        } else {
-            $('.totop').removeClass('on')
-            $('.Header').removeClass('on')
-        }
-    });
+  $(window).on("scroll", function () {
+    const sct = $(window).scrollTop();
+    if (sct > 0) {
+      $(".header").addClass("on");
+    } else {
+      $(".header").removeClass("on");
+    }
+  });
 });
